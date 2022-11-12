df3bb7ebbf09a2ab1047eb6fd36e932fef239ce6
AishaZhenisbekqz <117504731+AishaZhenisbekqz@users.noreply.github.com>
2022-11-12 12:22:06 +0600

    Update init.sh

--- init.sh
+++ init.sh
@@ -4,3 +4,5 @@ sudo /etc/init.d/nginx restart
 sudo ln -sf /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test-wsgi
 sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/test-django
 sudo /etc/init.d/gunicorn restart
+gunicorn -c ../etc/hello.py hello:app                                          
+gunicorn -c ../etc/django_conf.py ask.wsgi
