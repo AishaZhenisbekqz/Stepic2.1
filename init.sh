372d92308a718dcda8ac42c600c17bbb8bab6138
AishaZhenisbekqz <117504731+AishaZhenisbekqz@users.noreply.github.com>
2022-11-12 15:57:46 +0600

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,2 +1,6 @@
-sudo gunicorn -c /home/box/Stepic2.1/web/etc/gunicorn.conf hello:wsgi_application
-sudo gunicorn -c /home/box/Stepic2.1/web/etc/gunicorn-django.conf ask.wsgi:application
+sudo rm /etc/nginx/sites-enabled/default
+sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo /etc/init.d/nginx restart
+sudo ln -sf /home/box/web/etc/gunicorn-wsgi.conf /etc/gunicorn.d/test-wsgi
+sudo ln -sf /home/box/web/etc/gunicorn-django.conf /etc/gunicorn.d/test-django
+sudo /etc/init.d/gunicorn restart
